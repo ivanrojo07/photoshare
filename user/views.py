b3dab6ca463bf4999cad7409f753cfe7e7a3b5b7
Ivan Rojo <ivan@mail.com>
2021-01-25 18:38:06 -0600

    user.views def profile_viewwwwwwwwwwwwwwwww

--- user/views.py
+++ user/views.py
@@ -103,4 +103,47 @@ def accept_friend_request(request,id):
 @login_required
 def delete_friend_request(request,id):
     from_user = get_object_or_404(User,id=id)
-    f_request = FriendRequest.objects.filter(from_user=from_user,to_user=request)
+    f_request = FriendRequest.objects.filter(from_user=from_user,to_user=request.user).first()
+    f_request.delete()
+    return HttpResponseRedirect('/users/{}'.format(request.user.profile.slug))
+
+def delete_friend(request, id):
+    user_profile = request.user.profile
+    friend_profile = get_object_or_404(Profile,id=id)
+    user_profile.friends.remove(friend_profile)
+    friend_profile.friends.remove(user_profile)
+    return HttpResponseRedirect('/users/{}'.format(friend_profile.slug))
+
+@login_required
+def profile_view(request,slug):
+    profile = Profile.objects.filter(slug=slug).first()
+    user = profile.user
+    sent_friend_requests = FriendRequest.objects.filter(from_user = user)
+    rec_friend_requests = FriendRequest.objects.filter(to_user = user)
+    user_posts = Post.objects.filter(user_name=user)
+
+    friends = profile.friends.all()
+
+    # es este usuario dentro de amigos
+    button_status = 'none'
+
+    if p not in request.user.profile.friends.all():
+        button_status='not_friend'
+
+        # si ya hemos enviado la solicitud
+        if len(FriendRequest.objects.filter(from_user=request.user).filter(to_user=user)) == 1:
+            button_status = 'friend_request_sent'
+
+        # Si hemos recibido solicitud de amistad
+        if len(FriendRequest.objects.filter(from_user=user).filter(to_user=request.user))==1:
+            button_status="friend_request_received"
+
+    context = {
+        'u':user,
+        'button_status':button_status,
+        'friens_list':friends,
+        'sent_friend_requests':rec_friend_requests,
+        'post_count':user_posts.count
+    }
+
+    return render(request,"users/profile.html",context)
